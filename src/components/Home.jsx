25c07d41a76e5b0186c3d0f29cbf99fd37e7c1f5
Rakib-siddiki <rmrakib901@gmail.com>
2023-09-24 15:29:27 +0600

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+
+
+const Home = () => {
+    return (
+      <>
+        <div className="container">
+          <div className="text">
+            <h1>
+              {`I'M`} Rakib <span>Siddiki</span>
+            </h1>
+            <p>
+              Energetic MERN stack developer with a passion for learning new
+              technologies. Seeking a role where I can grow my skills and make a
+              difference in the world.
+            </p>
+            <a
+              href="https://docs.google.com/document/d/1hw_Y7m7ZHBxjM3WZTTl14vTCLBdyPopt/edit?usp=share_link&ouid=107622900685458882405&rtpof=true&sd=true"
+              
+            >
+              Download CV
+            </a>
+          </div>
+          <div className="images ">
+            <img src="images/shape.png" className="shape" />
+            <img src="images/boy.png" className="boy" />
+          </div>
+
+          <div className="social">
+            <a href="https://www.facebook.com/orakib901">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a
+              href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Frakib_e.mertinez%3Ffbclid%3DIwAR1gYGvaOvkSF2BXADVooSW0Tzxo2zdqw0azYAvCk9FwZQfAoQYRZH5gvDU&h=AT33VwDRDAQC8YzXtqvNfHK5nX1Xpm6Md-wqCf4y_xoelSYOaUEpe_LvzrDba3Ad4V0qAJnLlKdYE3xx-Hs-SIK1s1pvBR4lFQDRQxrmbd2HZhRP9oNFUOJaTI0PnM_8CY3O"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rakib-siddiki-543a28257/"
+              target='blank'
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+
+const Navbar = () => {
+    return (
+      <>
+        <nav className="navbar container navbar-expand-lg ">
+          <a className="navbar-brand" href="#">
+            <img src="images/logo.png" alt="" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <div className="nav_icon">
+              <i className="fa-solid fa-bars"></i>
+            </div>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item active ">
+                <a className="nav-link" href="#">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  ABOUT
+                </a>
+              </li>
+              <li className="nav-item dropdown active">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  PROTFOLIO
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a target="_black"
+                    className="dropdown-item"
+                    href=" https://quriarbox.netlify.app/"
+                  >
+                    Quriar box app
+                  </a>
+                  <a target="_blank"
+                    className="dropdown-item"
+                    href=" https://petriol-app.netlify.app/"
+                  >
+                    Petroil app
+                  </a>
+                  <a target="_blank"
+                    className="dropdown-item"
+                    href=" https://rakib-siddiki.github.io/Creative-it_landing-page/"
+                  >
+                    CREATIVE IT LANDIG-PAGE
+                  </a>
+                  <a className="dropdown-item"href="https://rakib-siddiki.github.io/Agenc/">
+                    AGENC WEBSITE FUll RESPONSIVE
+                  </a>
+                  <a 
+                    className="dropdown-item"
+                    href="https://rakib-siddiki.github.io/pack-go/"
+                  >
+                    PACK & GO PROJECT
+                  </a>
+                  <a 
+                    className="dropdown-item"
+                    href="
+            https://rakib-siddiki.github.io/Kuub_/"
+                  >
+                    
+                    PSD TO HTML
+                  </a>
+                  <a 
+                    className="dropdown-item"
+                    href="https://rakib-siddiki.github.io/Practice_project/"
+                  >
+                    PARCTICE-PROJECT
+                  </a>
+                </div>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  SERVICES
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link last-menu" href="#">
+                  HIRE ME
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    );
+};
+
+export default Navbar;
